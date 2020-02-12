7c5f6f91f6c072168db54c5014bdb75847727620
jamesperalta <jamesperalta35@gmail.com>
2020-02-12 11:48:31 -0700

    Fixed overflowing on the input and output text boxes

--- public/calculator-logic.js
+++ public/calculator-logic.js
@@ -1,63 +1,64 @@
-$(document).ready(function () {
+$(document).ready(function ()
+{
   let result = 0;
   let saved_result = false;
   let entry = "";
-  // Get original button height so we can tell if it overflows
-  $("#outputscreen h1").html("H");
-  let orig_output_height = $("#outputscreen h1").height();
-  $("#outputscreen h1").html("");
 
-  $("button").click(function () {
+  $("button").click(function ()
+  {
       let button_click = $(this).text();
 
-      if (button_click === "=") {
-        try {
-            result = result + parseFloat(eval(entry).toFixed(5));
-            $("#outputscreen h1").html(entry + " = "+ result);
-            entry = "";
-            saved_result = true;
-        }
-        catch (err) {
-            $("#outputscreen h1").html(entry + " = " + "ERROR");
-            entry = "";
-        }
-        if ($("#outputscreen h1").height() > orig_output_height) {
-            $("#outputscreen h1").css({
-                'font-size': "20px"
-            });
+      if (button_click === "=")
+      {
+        // Only evaluate if there is input typed in
+        if ($("#inputscreen h1").text() !== ""){
+            try
+            {
+                result = result + parseFloat(eval(entry).toFixed(5));
+                $("#outputscreen h1").html(entry + " = " + result);
+                entry = "";
+                saved_result = true;
+            }
+            catch (err)
+            {
+                $("#outputscreen h1").html(entry + " = " + "ERROR");
+                entry = "";
+            }
         }
       }
-      else if (button_click === "C"){
+      else if (button_click === "C")
+      {
         saved_result = false;
         entry = "";
         result = 0;
         $("#inputscreen h1").html("");
         $("#outputscreen h1").html("");
-        $("#outputscreen h1").css({
-          'font-size': "30px"
-        });
       }
-      else if (button_click === "CE"){
+      else if (button_click === "CE")
+      {
           entry = entry.substr(0, entry.length - 1);
       }
-      else {
-        if (saved_result && ["/", "*", "-", "+"].includes(button_click)) {
+      else
+      {
+        if (saved_result && ["/", "*", "-", "+"].includes(button_click))
+        {
           saved_result = false;
           entry = result + button_click;
           result = 0;
         }
-        else if (saved_result) {
+        else if (saved_result)
+        {
           saved_result = false;
           $("#outputscreen h1").html("ANS" + " = "+ result);
           entry = entry + button_click;
           result = 0;
         }
-        else {
+        else
+        {
           entry = entry + button_click;
         }
       }
       $("#inputscreen h1").html(entry);
     }
   )
-  }
-)
+});
